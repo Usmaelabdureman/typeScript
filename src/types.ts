364c7d75b2f579262f89c283936a51acde32505b
Usmaelabdureman <uabdureman@gmail.com>
2023-03-20 11:06:07 +0300

    add some change

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+/*
+There are three main primitives in JavaScript and TypeScript.
+
+boolean - true or false values
+number - whole numbers and floating point values
+string - text values like "TypeScript Rocks"
+
+*/ 
+
+//Explicit Type 
+//explicit type assignment are easier to read and more intentional.
+// let firstName: String ="Usmael";
+
+//Implicit Type
+//Implicit - TypeScript will "guess" the type, based on the assigned value:
+let lastName ="abdi";
+// check for boolean
+// let a: number=6
+const json = JSON.parse("55");
+console.log(typeof json)
+
